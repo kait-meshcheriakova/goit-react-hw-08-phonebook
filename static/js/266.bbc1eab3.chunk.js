1cf6f402b2e1a25ed443f2cbc101eef0c906f7fd
kait-meshcheriakova <129512123+kait-meshcheriakova@users.noreply.github.com>
2023-11-17 13:06:16 +0000

    Deploying to gh-pages from @ kait-meshcheriakova/goit-react-hw-08-phonebook@3f263abe4395d1a714aa7f469ed26f3ad2491448 🚀

--- static/js/266.bbc1eab3.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(824),c=n(2715),i=n(184);function o(){return(0,i.jsx)(r.xu,{minHeight:"calc(100vh - 50px)",alignItems:"center",justifyContent:"center",children:(0,i.jsx)(c.x,{fontWeight:" 500px",fontSize:"48px",textAlign:"center",bgGradient:"linear(to-l, #23eb27, #82d47b)",bgClip:"text",children:"Welcome to the phone book!"})})}}}]);
-//# sourceMappingURL=266.bbc1eab3.chunk.js.map
